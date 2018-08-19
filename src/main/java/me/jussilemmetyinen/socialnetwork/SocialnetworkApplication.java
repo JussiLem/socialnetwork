3d3e7a8e2b7c2e3484876947da6758ef5efe611f
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-08-19 20:30:52 +0300

    work in progress

--- src/main/java/me/jussilemmetyinen/socialnetwork/SocialnetworkApplication.java
+++ src/main/java/me/jussilemmetyinen/socialnetwork/SocialnetworkApplication.java
@@ -3,6 +3,7 @@ package me.jussilemmetyinen.socialnetwork;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SocialnetworkApplication {
 
