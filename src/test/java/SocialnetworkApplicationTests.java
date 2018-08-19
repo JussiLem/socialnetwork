112dec1ac9b2f85edffcd94a19ff0c85a58fb678
Jussi Lemmetyinen <jussi.lem@gmail.com>
2018-08-19 18:54:10 +0300

    Fixed the test

--- src/test/java/SocialnetworkApplicationTests.java
+++ src/test/java/SocialnetworkApplicationTests.java
@@ -1,10 +1,11 @@
+import me.jussilemmetyinen.socialnetwork.SocialnetworkApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SocialnetworkApplication.class)
 public class SocialnetworkApplicationTests {
 
 	@Test
